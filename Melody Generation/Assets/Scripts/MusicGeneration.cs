21d0997c7dd4576920e9fefce1239ef9466e41e4
DanielNeale <danielneale01@gmail.com>
2019-11-06 00:32:34 +0000

    Created script that plays a tone on a button press

--- Melody Generation/Assets/Scripts/MusicGeneration.cs
+++ Melody Generation/Assets/Scripts/MusicGeneration.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class MusicGeneration : MonoBehaviour
 {
-    void Start()
+    private AudioSource SOURCE;
+    private AudioClip song;
+    public TextAsset NOTES_TXT;
+    private Dictionary<string, float> NOTES;
+    private int line_num;
+
+    private void Start()
     {
-        
+        SOURCE = GetComponent<AudioSource>();
+
+        // Reference for frequency of musical notes
+        // https://pages.mtu.edu/~suits/notefreqs.html
+
+        for (int i = 0; i < NOTES_TXT.text.Length; i++)
+        {
+
+        }
     }
 
-    void Update()
+    public void PlayMusic()
     {
-        
+        int tone;
+
+        song = CreateToneAudioClip(150);
+
+        SOURCE.PlayOneShot(song);
+    }
+
+    private AudioClip CreateToneAudioClip(int frequency)
+    {
+        int sampleDurationSecs = 5;
+        int sampleRate = 44100;
+        int sampleLength = sampleRate * sampleDurationSecs;
+        float maxValue = 1f / 4f;
+
+        AudioClip audioClip = AudioClip.Create("song", sampleLength, 1, sampleRate, false);
+
+        float[] samples = new float[sampleLength];
+
+        for (int i = 0; i < sampleLength; i++)
+        {
+            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float)i / (float)sampleRate));
+            float v = s * maxValue;
+            samples[i] = v;
+        }
+
+        audioClip.SetData(samples, 0);
+        return audioClip;
     }
 }
